ae7e782d93aa9d9a0da7de81f17524f413569888
Amar Bansal <amarbansal28@gmail.com>
2021-06-06 00:26:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function whatFlavors(cost, money) {
     // Write your code here
-    let op = '';
+    let op = ''
     for(let i =0; i < cost.length; i++){
         for(let j =i+1; j < cost.length; j++){
             if(money == cost[i] + cost[j]){
-                op = cost[i] +' '+ cost[j];
+                j =j+1
+                console.log(i+1 +' '+ j);
             }
         }
     }
-    return op;
 }
+
 console.log(whatFlavors([4 ,3, 2, 5, 7],8))
 // o/p -> 3 5
